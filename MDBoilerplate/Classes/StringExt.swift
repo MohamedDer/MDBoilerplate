a614bb5c6ae5e7c92a2791d4d3a453c70b4097f5
MohamedDer <medder12@gmail.com>
2018-10-17 14:47:02 +0100

    Test config

---
+++ MDBoilerplate/Classes/StringExt.swift
@@ -0,0 +1,22 @@
+//
+//  StringExt.swift
+//  MDBoilerplate
+//
+//  Created by Mohamed Derkaoui on 17/10/2018.
+//
+
+import Foundation
+
+
+public class daString {
+    public var value: String?
+}
+
+
+extension String {
+    func doThat() -> String {
+        return "That's done !"
+    }
+}
+
+
